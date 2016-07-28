8afe53c29fa5de96225f6615984bc2b8ee5f3a3b
Kiea Seok Kang <grtlinux@gmail.com>
2016-07-29 07:34:27 +0900

    .

---
+++ TAIN_SYNKER/src/tain/kr/com/proj/synker/v04/main/client/ClientMain.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.synker.v04.main.client;
+
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.apache.log4j.Logger;
+
+import tain.kr.com.proj.synker.v04.bean.InfoBean;
+import tain.kr.com.proj.synker.v04.common.Version;
+import tain.kr.com.proj.synker.v04.utils.SynkerProperties;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : ClientMain.java
+ *   -. Package    : tain.kr.com.proj.synker.v04.main.client
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 7. 29. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class ClientMain {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(ClientMain.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static final String KEY_DESC = "tain.kr.main.desc";
+	
+	private String desc = null;
+	
+	public ClientMain() throws Exception {
+		
+		if (flag) {
+			String clsName = this.getClass().getName();
+			
+			ResourceBundle rb = ResourceBundle.getBundle(clsName.replace('.', '/'));
+			
+			this.desc = rb.getString(KEY_DESC);
+		}
+	}
+	
+	public String getDesc() throws Exception {
+		return this.desc;
+	}
+	
+	public void print() throws Exception {
+		if (flag) {
+			log.info("desc    > " + this.desc);
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	public int execute(String[] args) throws Exception {
+		
+		if (flag) {
+			/*
+			 * print version
+			 */
+			Version.getInstance().print();
+		}
+
+		if (flag) {
+			/*
+			 * print arguments
+			 */
+			for (String arg : args) {
+				log.debug("ARGS [" + arg + "]");
+			}
+		}
+		
+		if (flag) {
+			/*
+			 * print Synker.properties
+			 */
+			List<InfoBean> lstInfoBean = SynkerProperties.getInstance().getListInfoBean();
+			
+			if (flag) {
+				/*
+				 * print list of InfoBean
+				 */
+				
+				for (InfoBean bean : lstInfoBean) {
+					bean.print();
+				}
+			}
+		}
+
+		if (flag) {
+			/*
+			 * execute Synker Server
+			 */
+			// SynkerClient.getInstance().execute01();
+		}
+		
+		return 0;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static void test01(String[] args) throws Exception {
+		
+		if (!flag) {
+			/*
+			 * old version before 2016.06.22
+			 */
+			new ClientMain().print();
+			Version.getInstance().print();
+		}
+
+		if (flag) {
+			new ClientMain().execute(new String[] { "FileSynker" });
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		if (flag) log.debug(">>>>> " + new Object(){}.getClass().getEnclosingClass().getName());
+		
+		if (flag) test01(args);
+	}
+}
---
+++ TAIN_SYNKER/synker/shl/version.bat
@@ -0,0 +1,24 @@
+@echo off
+
+setlocal
+
+:: --------------------------------------------------------------
+set JAVA_HOME=M:\PROG\jdk1.7.0_79
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+:: --------------------------------------------------------------
+set TAIN_HOME=N:/WORK/GIT/GIT_DEPLOY1/TAIN_SYNKER/TAIN_SYNKER
+
+:: --------------------------------------------------------------
+set DEBUG=
+set DEBUG=%DEBUG% -Dtain.kr.main=tain.kr.com.proj.synker.v04.common.Version
+set DEBUG=%DEBUG% -Dtain.kr.synker.properties.file=N:/WORK/GIT/GIT_DEPLOY1/TAIN_SYNKER/TAIN_SYNKER/synker/conf/Synker.properties
+
+:: --------------------------------------------------------------
+java %DEBUG% -jar %TAIN_HOME%/synker/libs/tain-synker-1.0.jar
+
+:: --------------------------------------------------------------
+pause
+
+endlocal
+
