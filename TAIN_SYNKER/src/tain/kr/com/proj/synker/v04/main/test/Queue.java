da74b69b1bb5efb96e3c8e7f9fd5d7681392e593
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-02 22:34:15 +0900

    .

---
+++ TAIN_SYNKER/src/tain/kr/com/proj/synker/v04/main/test/Queue.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.synker.v04.main.test;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : Queue.java
+ *   -. Package    : tain.kr.com.proj.synker.v03.test
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 7. 29. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+@SuppressWarnings("unused")
+public class Queue {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(Queue.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
