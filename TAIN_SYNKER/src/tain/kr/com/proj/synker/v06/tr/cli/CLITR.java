6ecd26ca57c849d852c3b6b2e2a073c68df86e94
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-12 20:08:03 +0900

    .

---
+++ TAIN_SYNKER/src/tain/kr/com/proj/synker/v06/tr/cli/CLITR.java
@@ -0,0 +1,181 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.synker.v06.tr.cli;
+
+import org.apache.log4j.Logger;
+
+import tain.kr.com.proj.synker.v06.common.GlobalVars;
+import tain.kr.com.proj.synker.v06.stream.PipedStream;
+import tain.kr.com.proj.synker.v06.util.ThreadInvoke;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : CLITR.java
+ *   -. Package    : tain.kr.com.proj.synker.v05.main.test.v04
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 8. 9. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class CLITR extends Thread {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(CLITR.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private PipedStream ps = null;
+	
+	public CLITR(PipedStream ps) {
+	
+		if (flag) {
+			this.ps = ps;
+		}
+	}
+	
+	public void run() {
+		
+		if (flag) {
+			/*
+			 * parent thread, main thread
+			 */
+			
+			String req = "GET_DATE_TIME(" + (int) (Math.random() * 200) + ")";
+			String res = null;
+			
+			try {
+				
+				if (flag) log.debug(">>>>> REQ = [" + req + "]");
+
+				if (flag) {
+					/*
+					 * ReqWrite
+					 */
+					
+					ps.reqWrite(req);
+				}
+				
+				if (flag) {
+					/*
+					 * ResRead
+					 */
+					
+					res = ps.resRead();
+				}
+				
+				if (flag) log.debug(">>>>> RES = [" + res + "]\n");
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (flag) {
+			/*
+			 * close ps
+			 */
+			try { ps.close(); } catch (Exception e) {}
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * 2. Test
+	 */
+	private static void test01(String[] args) throws Exception {
+		
+		PipedStream ps = null;
+		
+		if (flag) {
+			/*
+			 * create piped stream object
+			 */
+			ps = new PipedStream();
+		}
+		
+		if (flag) {
+			/*
+			 * use class
+			 * elements class, constructor, run method
+			 */
+			// ThreadInvoke.execute(ps, "tain.kr.com.proj.synker.v05.main.test.v06.SVRTR");
+			ThreadInvoke.execute(ps, GlobalVars.getInstance().getSvrTrClass());
+		}
+
+		///////////////////////////////////////////////////
+		
+		if (flag) {
+			/*
+			 * parent thread, main thread
+			 */
+			
+			String req = "GET_DATE_TIME(" + (int) (Math.random() * 200) + ")";
+			String res = null;
+			
+			try {
+				
+				if (flag) log.debug(">>>>> REQ = [" + req + "]");
+
+				if (flag) {
+					/*
+					 * ReqWrite
+					 */
+					
+					ps.reqWrite(req);
+				}
+				
+				if (flag) {
+					/*
+					 * ResRead
+					 */
+					
+					res = ps.resRead();
+				}
+				
+				if (flag) log.debug(">>>>> RES = [" + res + "]");
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (flag) {
+			/*
+			 * close ps
+			 */
+			try { ps.close(); } catch (Exception e) {}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		if (flag) log.debug(">>>>> " + new Object(){}.getClass().getEnclosingClass().getName());
+		
+		if (flag) test01(args);
+	}
+}
