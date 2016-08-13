d4ee8280dd7e28dc46496edabc4e6625a82632cf
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-13 22:13:21 +0900

    .

--- TAIN_SYNKER/src/tain/kr/com/proj/synker/v06/tr/cli/CliTr0000.java
+++ TAIN_SYNKER/src/tain/kr/com/proj/synker/v06/tr/cli/CliTr0000.java
@@ -66,7 +66,7 @@ public class CliTr0000 extends Thread {
 			 * parent thread, main thread
 			 */
 			
-			String req = "GET_DATE_TIME(" + (int) (Math.random() * 200) + ")";
+			String req = "TR0000_(" + (int) (Math.random() * 200) + ")";
 			String res = null;
 			
 			try {
