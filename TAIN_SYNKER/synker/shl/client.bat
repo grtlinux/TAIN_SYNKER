14664867ec65ca2ddf8757bcf8d9ee135039d6dd
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-23 08:55:35 +0900

    .

--- TAIN_SYNKER/synker/shl/client.bat
+++ TAIN_SYNKER/synker/shl/client.bat
@@ -13,6 +13,7 @@ set TAIN_HOME=N:/WORK/GIT/GIT_DEPLOY1/TAIN_SYNKER/TAIN_SYNKER
 set DEBUG=
 set DEBUG=%DEBUG% -Dtain.kr.synker.conf.folder=%TAIN_HOME%/synker/conf
 set DEBUG=%DEBUG% -Dtain.kr.main.service=client
+set DEBUG=%DEBUG% -Dtain.kr.tr.list=TT0000,TR0000,
 
 :: --------------------------------------------------------------
 java %DEBUG% -jar %TAIN_HOME%/synker/libs/tain-synker-1.0.jar REAL
