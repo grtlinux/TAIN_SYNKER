07e81da47e3807683a3a4689723a12f4e3d51ed6
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-23 04:36:50 +0900

    .

---
+++ TAIN_SYNKER/src/tain/kr/com/proj/design/pattern/ch02Adapter/v01/PrintBanner.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.design.pattern.ch02Adapter.v01;
+
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : PrintBanner.java
+ *   -. Package    : tain.kr.com.proj.design.pattern.ch02Adapter.v01
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 8. 23. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class PrintBanner extends Banner implements Print{
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public PrintBanner(String string) {
+		super(string);
+	}
+
+	@Override
+	public void printWeak() {
+		showWithParen();
+	}
+
+	@Override
+	public void printStrong() {
+		showWithAster();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
