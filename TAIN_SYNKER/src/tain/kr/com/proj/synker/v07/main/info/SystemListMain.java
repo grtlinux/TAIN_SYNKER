ec01d701a545bc897e28bf4eec06ee2ab4b70f86
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-23 09:38:46 +0900

    .

---
+++ TAIN_SYNKER/src/tain/kr/com/proj/synker/v07/main/info/SystemListMain.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.synker.v07.main.info;
+
+import org.apache.log4j.Logger;
+
+import tain.kr.com.proj.synker.v07.base.common.GlobalParam;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : TrListMain.java
+ *   -. Package    : tain.kr.com.proj.synker.v07.main.info
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 8. 23. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class SystemListMain {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(SystemListMain.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static void clientModule() throws Exception {
+		
+		if (flag) {
+			/*
+			 * test for loop
+			 */
+			String[] trList = GlobalParam.getInstance().getTrList();
+			
+			if (trList != null) {
+				for (String trCode : trList) {
+					log.debug(">>>>> TRCODE = " + trCode);
+				}
+			}
+		}
+		
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static void test01(String[] args) throws Exception {
+		
+		if (flag) {
+			/*
+			 * print for checking arguments
+			 */
+			for (String arg : args) {
+				log.debug("ARG [" + arg + "]");
+			}
+		}
+
+		if (flag) clientModule();
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		if (flag) log.debug(">>>>> " + new Object(){}.getClass().getEnclosingClass().getName());
+		
+		if (args.length == 0) {
+			args = new String[] { "TEST-2" };
+		}
+
+		if (flag) test01(args);
+	}
+}
---
+++ TAIN_SYNKER/synker/shl/syslist.bat
@@ -0,0 +1,24 @@
+@echo off
+
+setlocal
+
+:: --------------------------------------------------------------
+set JAVA_HOME=N:\PROG\jdk1.7.0_79
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+:: --------------------------------------------------------------
+set TAIN_HOME=N:/WORK/GIT/GIT_DEPLOY1/TAIN_SYNKER/TAIN_SYNKER
+
+:: --------------------------------------------------------------
+set DEBUG=
+set DEBUG=%DEBUG% -Dtain.kr.synker.conf.folder=%TAIN_HOME%/synker/conf
+set DEBUG=%DEBUG% -Dtain.kr.main.service=syslist
+
+:: --------------------------------------------------------------
+java %DEBUG% -jar %TAIN_HOME%/synker/libs/tain-synker-1.0.jar REAL
+
+:: --------------------------------------------------------------
+pause
+
+endlocal
+
