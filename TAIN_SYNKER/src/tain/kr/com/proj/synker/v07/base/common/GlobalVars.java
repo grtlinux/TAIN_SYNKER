a1fb8a3e9122979da9a1243b6996576c4657c202
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-23 09:30:44 +0900

    .

--- TAIN_SYNKER/src/tain/kr/com/proj/synker/v07/base/common/GlobalVars.java
+++ TAIN_SYNKER/src/tain/kr/com/proj/synker/v07/base/common/GlobalVars.java
@@ -19,6 +19,9 @@
  */
 package tain.kr.com.proj.synker.v07.base.common;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -43,47 +46,38 @@ public class GlobalVars {
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
-	private String trCode = null;
-	private String cliTrClass = null;
-	private String svrTrClass = null;
-
+	private final Map<String, String> mapVars;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
-	private GlobalVars() {}
+	private GlobalVars() {
+		
+		this.mapVars = new HashMap<String, String>();
+	}
 	
-	public String getTrCode() {
-		return trCode;
+	public void put(String key, String value) {
+		this.mapVars.put(key, value);
 	}
-
-	public String getCliTrClass() {
-		return cliTrClass;
+	
+	public String get(String key) {
+		return this.get(key);
 	}
-
-	public String getSvrTrClass() {
-		return svrTrClass;
-	}
-
-	public void setTrCode(String trCode) {
-		this.trCode = trCode;
-	}
-
-	public void setCliTrClass(String cliTrClass) {
-		this.cliTrClass = cliTrClass;
-	}
-
-	public void setSvrTrClass(String svrTrClass) {
-		this.svrTrClass = svrTrClass;
-	}
-
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
 	public String toString() {
 		
 		StringBuffer sb = new StringBuffer();
+		sb.append("############################## GlobalParam ##############################\n");
+		
+		for (Map.Entry<String, String> entry : this.mapVars.entrySet()) {
+			String key = entry.getKey();
+			String value = entry.getValue();
+			
+			sb.append(String.format("\t[%s] => [%s]\n", key, value));
+		}
+		
 		sb.append("\n");
-		sb.append(String.format("\t[trCode    =%s]\n", trCode));
-		sb.append(String.format("\t[cliTrClass=%s]\n", cliTrClass));
-		sb.append(String.format("\t[svrTrClass=%s]\n", svrTrClass));
 		
 		return sb.toString();
 	}
@@ -111,6 +105,34 @@ public class GlobalVars {
 	private static void test01(String[] args) throws Exception {
 		
 		if (flag) {
+			/*
+			 * set values of the GlobalVars 
+			 */
+			GlobalVars.getInstance().put("author.name", "Seok Kang");
+			GlobalVars.getInstance().put("author.age", "33");
+			GlobalVars.getInstance().put("author.comment", "Programmer");
+		}
+		
+		if (flag) {
+			/*
+			 * print the list of the GlobalVars
+			 */
+			GlobalVars.getInstance().print();
+		}
+		
+		if (flag) {
+			/*
+			 * change values of the GlobalVars 
+			 */
+			GlobalVars.getInstance().put("author.age", "30");
+			GlobalVars.getInstance().put("author.hobby", "playing");
+			GlobalVars.getInstance().put("author.name", "Kiea Seok Kang");
+		}
+		
+		if (flag) {
+			/*
+			 * print the list of the GlobalVars
+			 */
 			GlobalVars.getInstance().print();
 		}
 	}
