b04fddf752ba5d542be6dafaade1dc30f269d394
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-24 11:40:18 +0900

    .

--- TAIN_SYNKER/src/tain/kr/com/proj/synker/v07/base/bean/FileEntryBean.java
+++ TAIN_SYNKER/src/tain/kr/com/proj/synker/v07/base/bean/FileEntryBean.java
@@ -19,6 +19,11 @@
  */
 package tain.kr.com.proj.synker.v07.base.bean;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
 /**
  * Code Templates > Comments > Types
  *
@@ -40,13 +45,18 @@ public class FileEntryBean {
 	/*
 	 * main key of mapFileEntry
 	 */
+	private final String mapKey;       // key of map
+	
+	/*
+	 * entry information 1
+	 */
 	private final String systemName;   // System Name
 	private final String gateName;     // Gate Name
 	private final String childPath;    // Child Path or Sub Path
 	private final String fileName;     // File Name
 	
 	/*
-	 * entry information
+	 * entry information 2
 	 */
 	private final long size;          // File size
 	private final long date;          // File modified date
@@ -62,6 +72,8 @@ public class FileEntryBean {
 		this.gateName = gateName;
 		this.childPath = childPath;
 		this.fileName = fileName;
+		
+		this.mapKey = this.systemName + this.gateName + this.childPath + this.fileName;
 		
 		this.size = size;
 		this.date = date;
@@ -104,7 +116,62 @@ public class FileEntryBean {
 		return step;
 	}
 	
+	public String getMapKey() {
+		return this.mapKey;
+	}
+	
+	public String toString() {
+		return String.format("[KEY,SYS,GATE,PATH,NAME,SIZ,DAT,CRC,STP] = [%s, %s, %s, %s, %s, %d, %d, %d, %c]"
+				, this.mapKey
+				, this.systemName
+				, this.gateName
+				, this.childPath
+				, this.fileName
+				, this.size
+				, this.date
+				, this.crc
+				, this.step
+				);
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(FileEntryBean.class);
+
+	private static void test01(String[] args) throws Exception {
+		
+		if (!flag) {
+			for (int i=0; i < 20; i++) {
+				System.out.format("[%3d]%n", (int) (Math.random() * 100));
+			}
+		}
+		
+		if (flag) {
+			// create
+			Map<String, FileEntryBean> mapFileEntry = new HashMap<String, FileEntryBean>();
+			
+			// put beans into map
+			for (int i=0; i < 100; i++) {
+				FileEntryBean bean = new FileEntryBean("SYSTEM", "GATE", "SUBPATH", String.format("FILENAME_%02d", (int)(Math.random() * 50)), 12345, 67890, 0, 'C');
+				mapFileEntry.put(bean.getMapKey(), bean);
+			}
+			
+			for (Map.Entry<String, FileEntryBean> entry : mapFileEntry.entrySet()) {
+				System.out.format("%s%n", entry.getValue());
+			}
+			
+			System.out.format("##### mapFileEntry.size() = %d", mapFileEntry.size());
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		if (flag) log.debug(">>>>> " + new Object(){}.getClass().getEnclosingClass().getName());
+		
+		if (flag) test01(args);
+	}
 }
