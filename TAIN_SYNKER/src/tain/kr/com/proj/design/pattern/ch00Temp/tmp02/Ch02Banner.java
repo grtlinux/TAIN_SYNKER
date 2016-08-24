aa91c5fa971d1786d9a8e7bbe98ec2b9605ccc1c
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-25 05:53:50 +0900

    .

---
+++ TAIN_SYNKER/src/tain/kr/com/proj/design/pattern/ch00Temp/tmp02/Ch02Banner.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.design.pattern.ch00Temp.tmp02;
+
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : Ch02Banner.java
+ *   -. Package    : tain.kr.com.proj.design.pattern.ch00Temp.tmp02
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 8. 25. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class Ch02Banner {
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private final String string;
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public Ch02Banner(String string) {
+		this.string = string;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	public String getString() {
+		return this.string;
+	}
+	
+	public String toString() {
+		return String.format("%s", this.getString());
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	public void showWithParen() {
+		System.out.println("(" + this.getString() + ")");
+	}
+	
+	public void showWithAster() {
+		System.out.println("*" + this.getString() + "*");
+	}
+}
---
+++ TAIN_SYNKER/src/tain/kr/com/proj/design/pattern/ch00Temp/tmp02/Ch02TestMain.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.design.pattern.ch00Temp.tmp02;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : Ch02TestMain.java
+ *   -. Package    : tain.kr.com.proj.design.pattern.ch00Temp.tmp02
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 8. 25. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class Ch02TestMain {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(Ch02TestMain.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static void test01(String[] args) throws Exception {
+		
+		if (flag) {
+			Ch02Print print = new Ch02PrintBanner("Hello");
+			print.printWeak();
+			print.printStrong();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		if (flag) log.debug(">>>>> " + new Object(){}.getClass().getEnclosingClass().getName());
+		
+		if (flag) test01(args);
+	}
+}
