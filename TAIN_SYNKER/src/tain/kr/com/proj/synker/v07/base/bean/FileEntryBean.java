6d407c0b3ad3d57a0294c6fae3507428ca7443df
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-24 11:11:53 +0900

    .

--- TAIN_SYNKER/src/tain/kr/com/proj/synker/v07/base/bean/FileEntryBean.java
+++ TAIN_SYNKER/src/tain/kr/com/proj/synker/v07/base/bean/FileEntryBean.java
@@ -19,8 +19,6 @@
  */
 package tain.kr.com.proj.synker.v07.base.bean;
 
-import org.apache.log4j.Logger;
-
 /**
  * Code Templates > Comments > Types
  *
@@ -37,12 +35,76 @@ import org.apache.log4j.Logger;
  */
 public class FileEntryBean {
 
-	private static boolean flag = true;
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * main key of mapFileEntry
+	 */
+	private final String systemName;   // System Name
+	private final String gateName;     // Gate Name
+	private final String childPath;    // Child Path or Sub Path
+	private final String fileName;     // File Name
+	
+	/*
+	 * entry information
+	 */
+	private final long size;          // File size
+	private final long date;          // File modified date
+	private final long crc;           // File crc value
+	
+	private final char step;           // Step : (1)Upload / (2)Download / (3)Check
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public FileEntryBean(String systemName, String gateName, String childPath, String fileName, long size, long date, long crc, char step) {
+		
+		this.systemName = systemName;
+		this.gateName = gateName;
+		this.childPath = childPath;
+		this.fileName = fileName;
+		
+		this.size = size;
+		this.date = date;
+		this.crc = crc;
+		
+		this.step = step;
+	}
 
-	private static final Logger log = Logger.getLogger(FileEntryBean.class);
+	///////////////////////////////////////////////////////////////////////////////////////////////
 
+	public String getSystemName() {
+		return systemName;
+	}
+
+	public String getGateName() {
+		return gateName;
+	}
+
+	public String getChildPath() {
+		return childPath;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public long getSize() {
+		return size;
+	}
+
+	public long getDate() {
+		return date;
+	}
+
+	public long getCrc() {
+		return crc;
+	}
+
+	public char getStep() {
+		return step;
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
-
 }
