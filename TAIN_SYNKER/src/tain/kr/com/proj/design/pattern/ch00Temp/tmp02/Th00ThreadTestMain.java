f504e48bdaa9b5b9b557ffc63345e2a6af861454
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-26 07:57:21 +0900

    .

---
+++ TAIN_SYNKER/src/tain/kr/com/proj/design/pattern/ch00Temp/tmp02/Th00ThreadTestMain.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.design.pattern.ch00Temp.tmp02;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : Th00ThreadTestMain.java
+ *   -. Package    : tain.kr.com.proj.design.pattern.ch00Temp.tmp02
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 8. 26. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class Th00ThreadTestMain {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(Th00ThreadTestMain.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static class MyThread extends Thread {
+		
+		private final String name;
+		
+		public MyThread(String name) {
+			this.name = name;
+		}
+		
+		public void run() {
+			System.out.println("THREAD-1 start thread...[name=" + this.name + "]");
+			try { Thread.sleep(1000); } catch (InterruptedException e) {}
+			System.out.println("THREAD-1 end   thread...[name=" + this.name + "]");
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static class MyRunnable implements Runnable {
+		
+		private final String name;
+		
+		public MyRunnable(String name) {
+			this.name = name;
+		}
+		
+		public void run() {
+			System.out.println("RUNNABLE-3 start thread...[name=" + this.name + "]");
+			try { Thread.sleep(1000); } catch (InterruptedException e) {}
+			System.out.println("RUNNABLE-3 end   thread...[name=" + this.name + "]");
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static void test01(String[] args) throws Exception {
+		
+		if (flag) {
+			Thread thread = new MyThread("KANG-1");
+			thread.start();
+			thread.join();
+		}
+		
+		if (flag) {
+			Thread thread = new Thread() {
+				public void run() {
+					System.out.println("THREAD-2 start thread...");
+					try { Thread.sleep(1000); } catch (InterruptedException e) {}
+					System.out.println("THREAD-2 end   thread...");
+				}
+			};
+			thread.start();
+			thread.join();
+		}
+		
+		if (flag) {
+			Thread thread = new Thread(new MyRunnable("KANG-3"));
+			thread.start();
+			thread.join();
+		}
+		
+		if (flag) {
+			Thread thread = new Thread(new Runnable(){
+				public void run() {
+					System.out.println("RUNNABLE-4 start thread...");
+					try { Thread.sleep(1000); } catch (InterruptedException e) {}
+					System.out.println("RUNNABLE-4 end   thread...");
+				}
+			});
+			thread.start();
+			thread.join();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		if (flag) log.debug(">>>>> " + new Object(){}.getClass().getEnclosingClass().getName());
+		
+		if (flag) test01(args);
+	}
+}
