4429a1d84f6b1a91f133ce842cad4542a869c500
Kiea Seok Kang <grtlinux@gmail.com>
2016-09-11 21:16:59 +0900

    .

--- TAIN_SYNKER/src/tain/kr/com/proj/design/pattern/ch07Builder/v01/Director.java
+++ TAIN_SYNKER/src/tain/kr/com/proj/design/pattern/ch07Builder/v01/Director.java
@@ -19,16 +19,12 @@
  */
 package tain.kr.com.proj.design.pattern.ch07Builder.v01;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 
 /**
  * Code Templates > Comments > Types
  *
  * <PRE>
- *   -. FileName   : HtmlBuilder.java
+ *   -. FileName   : Director.java
  *   -. Package    : tain.kr.com.proj.design.pattern.ch07Builder.v01
  *   -. Comment    :
  *   -. Author     : taincokr
@@ -38,59 +34,41 @@ import java.io.PrintWriter;
  * @author taincokr
  *
  */
-public class HtmlBuilder extends Builder {
+public class Director {
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
-	private final String fileName;
-	private PrintWriter writer;
+	private final Builder builder;
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
-	public HtmlBuilder() {
-		this.fileName = "N:/hello.html";
-		
-		try {
-			this.writer = new PrintWriter(new FileWriter(this.fileName));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public Director(Builder builder) {
+		this.builder = builder;
 	}
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
-	@Override
-	public void makeTitle(String title) {
+	public Object construct() {
 		
-		this.writer.println("<html><head><title>" + title + "</title></head><body>");
+		this.builder.makeTitle("Greeting");
 		
-		this.writer.println("<h1>" + title + "</h1>");
-	}
-	
-	@Override
-	public void makeString(String string) {
-		this.writer.println("<p>" + string + "</p>");
-	}
-	
-	@Override
-	public void makeItems(String[] items) {
-		this.writer.println("<ul>");
+		this.builder.makeString("아침과 낮에");
 		
-		for (String item : items) {
-			this.writer.println("<li>" + item + "</li>");
-		}
-
-		this.writer.println("</ul>");
-	}
-	
-	@Override
-	public Object getResult() {
-		this.writer.println("</body></html>");
+		this.builder.makeItems(new String[] {
+				"좋은 아침입니다.",
+				"안녕하세요.",
+		});
 		
-		this.writer.close();
-		return this.fileName;
+		this.builder.makeString("밤에");
+		
+		this.builder.makeItems(new String[] {
+				"안녕하세요.",
+				"안녕히 주무세요.",
+				"안녕히 계세요.",
+		});
+		
+		return this.builder.getResult();
 	}
-	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
--- TAIN_SYNKER/src/tain/kr/com/proj/design/pattern/ch07Builder/v01/TextBuilder.java
+++ TAIN_SYNKER/src/tain/kr/com/proj/design/pattern/ch07Builder/v01/TextBuilder.java
@@ -19,16 +19,12 @@
  */
 package tain.kr.com.proj.design.pattern.ch07Builder.v01;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 
 /**
  * Code Templates > Comments > Types
  *
  * <PRE>
- *   -. FileName   : HtmlBuilder.java
+ *   -. FileName   : TextBuilder.java
  *   -. Package    : tain.kr.com.proj.design.pattern.ch07Builder.v01
  *   -. Comment    :
  *   -. Author     : taincokr
@@ -38,57 +34,49 @@ import java.io.PrintWriter;
  * @author taincokr
  *
  */
-public class HtmlBuilder extends Builder {
+public class TextBuilder extends Builder {
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
-	private final String fileName;
-	private PrintWriter writer;
+	private final StringBuffer buffer;
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
-	public HtmlBuilder() {
-		this.fileName = "N:/hello.html";
-		
-		try {
-			this.writer = new PrintWriter(new FileWriter(this.fileName));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public TextBuilder() {
+		this.buffer = new StringBuffer();
 	}
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
 	@Override
 	public void makeTitle(String title) {
-		
-		this.writer.println("<html><head><title>" + title + "</title></head><body>");
-		
-		this.writer.println("<h1>" + title + "</h1>");
+
+		this.buffer.append("========================================================================\n");
+		this.buffer.append("<" + title + ">\n");
+		this.buffer.append("\n");
 	}
 	
 	@Override
 	public void makeString(String string) {
-		this.writer.println("<p>" + string + "</p>");
+		
+		this.buffer.append("*" + string + "\n");
+		this.buffer.append("\n");
 	}
 	
 	@Override
 	public void makeItems(String[] items) {
-		this.writer.println("<ul>");
 		
 		for (String item : items) {
-			this.writer.println("<li>" + item + "</li>");
+			this.buffer.append("-" + item + "\n");
 		}
-
-		this.writer.println("</ul>");
+		this.buffer.append("\n");
 	}
 	
 	@Override
 	public Object getResult() {
-		this.writer.println("</body></html>");
 		
-		this.writer.close();
-		return this.fileName;
+		this.buffer.append("========================================================================\n");
+		return this.buffer.toString();
 	}
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////
